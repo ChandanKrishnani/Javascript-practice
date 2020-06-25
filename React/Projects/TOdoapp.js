47ff662d2c5a38229e6a419a4aa7963830bf0a50
Chandan krishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2020-06-25 17:20:53 +0530

    Update TOdoapp.js
    It is an app which will help users  what to do and the counts of their done work and remaining works

--- React/Projects/TOdoapp.js
+++ React/Projects/TOdoapp.js
@@ -1,1 +1,63 @@
-//sooon will be pubclished 
+import React from 'react';
+import { render } from "react-dom";
+let id =0;
+const Todo = props => (
+  <li>
+    <input type="checkbox" checked={props.todo.checked} onChange={props.onToggle} />
+    <button onClick={props.ondelete}>delete</button>
+    <span>{props.todo.text}</span>
+    <span>{props.todo.id}</span>
+  </li>
+)
+
+
+class App extends React.Component{
+  constructor()
+  {
+    super()
+    this.state={
+      todos: [],
+    }
+  }
+  addTodo()
+  {
+    const text = prompt("ToDo text please : ")
+    this.setState({todos : [...this.state.todos, {id: ++id,text: text, checked :false}],
+    })
+  }
+  removeTodo(id)
+  {
+    this.setState({ todos : this.state.todos.filter(todo => todo.id !== id )})
+
+  }
+  toggleTodo(id)
+  {
+    this.setState({todos : this.state.todos.map (todo => {
+      if(todo.id !== id) return todo
+      return {
+        id :todo.id,
+        text :todo.text,
+        checked :!todo.checked,
+      }
+    })})
+
+  }
+  render(){
+    return (
+      <div>
+        <div>Todo count: {this.state.todos.length}</div> 
+        <div> Unchecked count :{this.state.todos.filter(todo => !todo.checked).length}</div>
+        <button onClick={() => this.addTodo()}>Add TODO</button>
+        <ul>
+          {this.state.todos.map(todo => (<Todo 
+           onToggle={() => this.toggleTodo(todo.id)}
+          ondelete={() => this.removeTodo(todo.id)}
+          todo={todo} 
+          />))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+render(<App />,document.getElementById('root'));
