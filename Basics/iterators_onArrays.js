85c31c4aa09c5933ba0b952b57e8fc95d75e42ee
Chandan krishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2021-08-18 17:41:07 +0530

    Create iterators_onArrays.js

---
+++ Basics/iterators_onArrays.js
@@ -0,0 +1,13 @@
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some((word) => {
+  return word.length < 6;
+}));
+const interestingWords=words.filter(word => {
+  return word.length>5;
+});
+
+
+console.log(interestingWords.every((word) => { return word.length > 5; }));
