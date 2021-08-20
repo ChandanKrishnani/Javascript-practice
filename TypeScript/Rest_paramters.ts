e74b723c93166c0d78ff1c68927196975262d37d
Chandan krishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2021-08-20 12:11:17 +0530

    Create Rest_paramters.ts

---
+++ TypeScript/Rest_paramters.ts
@@ -0,0 +1,11 @@
+function addPower(p:number, ...numsToAdd:number[]): number{
+  let answer = 0; 
+  for(let i = 0; i < numsToAdd.length; i++)
+  {
+    answer += numsToAdd[i] ** p;
+  }
+  return answer;
+}
+addPower('astring',4,5,6);
+
+
