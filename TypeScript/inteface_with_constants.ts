96b7185ed474d138a853594b3deede2e67014fa0
Chandan krishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2021-08-24 17:09:53 +0530

    Create inteface_with_constants.ts

---
+++ TypeScript/inteface_with_constants.ts
@@ -0,0 +1,15 @@
+interface cars 
+{
+    model:string,
+    chasis:string,
+    color:string,
+    reg_no: string | number
+};
+const car : cars ={
+    model:'i20',
+    chasis:'abc123',
+    color:'red',
+    reg_no:1739479
+
+}
+console.log(car);
